a6bd473d0604a59419f06a5055326b19cc06e4bc
Isaac Talb <thaungahlaybaw@gmail.com>
2024-11-21 00:41:03 +0700

    removed unused useState

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Aside = () => {
   return (
     <aside className="bg-gray-100 p-4 w-1/4">
@@ -7,17 +8,17 @@ const Aside = () => {
         <ul>
           <li className="mb-2">
             <a href="#" className="text-blue-500">
-              Menu Item 1
+              Side Bar
             </a>
           </li>
           <li className="mb-2">
             <a href="#" className="text-blue-500">
-              Menu Item 2
+              Navigation
             </a>
           </li>
           <li className="mb-2">
             <a href="#" className="text-blue-500">
-              Menu Item 3
+              Footer
             </a>
           </li>
         </ul>
